7de8840e3eca181e2e0295ca9684ad6546e60ae8
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-08-01 18:34:56 -0300

    :bug: fix: empty created at and updated at columns in user creation.

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,16 +1,20 @@
 package domain
 
 import (
+	"time"
+
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type User struct {
+	gorm.Model
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	Name      string    `json:"name" validate:"required,min=3,max=100"`
 	Email     string    `json:"email" validate:"required,email"`
 	Password  string    `json:"password" validate:"required,min=6,max=100"`
-	CreatedAt string    `json:"created_at"`
-	UpdatedAt string    `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserRepository interface {
