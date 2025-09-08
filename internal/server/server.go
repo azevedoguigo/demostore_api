a87619c7862904ce733447ed721ee627199b9d96
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-09-08 16:11:17 -0300

    :sparkles: feat: get product by id.

--- internal/server/server.go
+++ internal/server/server.go
@@ -58,6 +58,7 @@ func (s *Server) SetupRoutes() {
 	s.router.Route("/api/v1/products", func(r chi.Router) {
 		r.Post("/", productHandler.CreateProduct)
 		r.Get("/", productHandler.GetAllProducts)
+		r.Get("/{id}", productHandler.GetProductByID)
 	})
 }
 
