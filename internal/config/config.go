d5e80d9cd9ecf8c30419fb2e9aae29db54420ab7
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-11-26 19:26:06 -0300

    :recycle: refactor: get envs functions moved to utils package.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,9 +2,8 @@ package config
 
 import (
 	"log"
-	"os"
-	"strconv"
 
+	"github.com/azevedoguigo/demostore_api.git/pkg/utils"
 	"github.com/joho/godotenv"
 )
 
@@ -24,34 +23,12 @@ func LoadConfig() *Config {
 	}
 
 	return &Config{
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnvAsInt("DB_PORT", 5432),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "postgres"),
-		DBName:     getEnv("DB_NAME", "postgres"),
-		DBSSLMode:  getEnv("DB_SSL_MODE", "disable"),
-		ServerPort: getEnv("SERVER_PORT", "8080"),
+		DBHost:     utils.GetEnv("DB_HOST", "localhost"),
+		DBPort:     utils.GetEnvAsInt("DB_PORT", 5432),
+		DBUser:     utils.GetEnv("DB_USER", "postgres"),
+		DBPassword: utils.GetEnv("DB_PASSWORD", "postgres"),
+		DBName:     utils.GetEnv("DB_NAME", "postgres"),
+		DBSSLMode:  utils.GetEnv("DB_SSL_MODE", "disable"),
+		ServerPort: utils.GetEnv("SERVER_PORT", "8080"),
 	}
-}
-
-func getEnv(key, defaultValue string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		return defaultValue
-	}
-	return value
-}
-
-func getEnvAsInt(key string, defaultValue int) int {
-	valueStr := getEnv(key, "")
-	if valueStr == "" {
-		return defaultValue
-	}
-
-	value, err := strconv.Atoi(valueStr)
-	if err != nil {
-		log.Printf("Error converting environment variable %s to int: %v", key, err)
-		return defaultValue
-	}
-	return value
 }
