5e416440ba0779cb876b82e8d525da9592203f56
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-11-26 18:27:10 -0300

    :recycle: refactor: change database default connection values.

--- internal/config/config.go
+++ internal/config/config.go
@@ -26,8 +26,8 @@ func LoadConfig() *Config {
 	return &Config{
 		DBHost:     getEnv("DB_HOST", "localhost"),
 		DBPort:     getEnvAsInt("DB_PORT", 5432),
-		DBUser:     getEnv("DB_USER", "user"),
-		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
 		DBName:     getEnv("DB_NAME", "postgres"),
 		DBSSLMode:  getEnv("DB_SSL_MODE", "disable"),
 		ServerPort: getEnv("SERVER_PORT", "8080"),
